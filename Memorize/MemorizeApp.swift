367b6686c652311fdc807a29cb148d2f7abc4ea2
terry-koo <xpflxhfl94@naver.com>
2022-04-03 23:46:51 +0900

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Terry Koo on 2022/04/03.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
